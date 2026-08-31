51a391df2ec8ddf5167c784a1ec1ff47c787f25c
Lucas Fernandes | Full Stack Developer <akaakashiseijuro@gmail.com>
2026-08-31 19:27:36 -0300

    feat(android): register Live tool bridge

--- apps/android-capacitor/native/java/MainActivity.java
+++ apps/android-capacitor/native/java/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         registerPlugin(AssistantBridgePlugin.class);
+        registerPlugin(LiveToolBridgePlugin.class);
         super.onCreate(savedInstanceState);
         if (getBridge() != null && getBridge().getWebView() != null) {
             getBridge().getWebView().getSettings().setMediaPlaybackRequiresUserGesture(false);
