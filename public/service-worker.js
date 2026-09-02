3417500046fbd9aa5c0a843668e16e2b646773a0
Lucas Fernandes | Full Stack Developer <akaakashiseijuro@gmail.com>
2026-09-01 23:10:58 -0300

    chore(pwa): refresh cache for Voice Core v9

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'sexta-2.3-voice-core-v8-manual-vad';
+const CACHE = 'sexta-2.4-voice-core-v9-gemini31';
 const ASSETS = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.webmanifest', '/icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
